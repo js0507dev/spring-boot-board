9bf76ca1eb6df138b11f8dd4cdd5e4ba49c0eab0
js0507dev <js0507dev@gmail.com>
2019-08-15 21:48:07 +0900

    UserRepository 테스트작성 완료

--- src/main/java/js/spring/boot/board/model/User.java
+++ src/main/java/js/spring/boot/board/model/User.java
@@ -1,23 +1,45 @@
 package js.spring.boot.board.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
+import java.sql.Date;
+import java.time.LocalDate;
 
-@Data
 @Entity
-@Builder
-@Table(name = "USER")
+@Getter
+@Setter
+@ToString
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", length = 40)
+    @Column(length = 40)
     private String id;
-    @Column(name = "NAME", length = 30)
+    @Column(length = 30)
     private String name;
-    @Column(name = "PASSWORD", length = 50)
+    @Column(length = 50)
     private String password;
-    @Column(name = "EMAIL_ADDR", length = 100)
+    @Column(length = 100)
     private String emailAddr;
+    private LocalDate regDate;
+
+    @Builder
+    public User(String id, String name, String password, String emailAddr) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.emailAddr = emailAddr;
+        this.regDate = LocalDate.now();
+    }
+
+    public User(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.password = user.getPassword();
+        this.emailAddr = user.getEmailAddr();
+        this.regDate = user.getRegDate();
+    }
+
+    public User() {
+        this("", "", "", "");
+    }
 }
--- src/test/java/js/spring/boot/board/repository/UserRepositoryTest.java
+++ src/test/java/js/spring/boot/board/repository/UserRepositoryTest.java
@@ -1,17 +1,18 @@
 package js.spring.boot.board.repository;
 
 import js.spring.boot.board.model.User;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,43 +20,68 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    private User tempUser;
+    private List<User> userList;
 
     @Before
-    private void setup() throws Exception {
-        tempUser.builder()
+    public void setup() throws Exception {
+        userList = new ArrayList<>();
+
+        User user1 = User.builder()
                 .id("test1")
                 .name("testuser")
-                //.password()
-                .emailAddr("test@email.com");
+                .password("test")
+                .emailAddr("test@email.com")
+                .build();
+
+        User user2 = User.builder()
+                .id("test2")
+                .name("testuser22")
+                .password("test22")
+                .emailAddr("test2@email.com")
+                .build();
+
+        userList.add(user1);
+        userList.add(user2);
     }
 
     @Test
-    public void addUserTest() throws Exception {
-        userRepository.save(tempUser);
-        List<User> list = userRepository.findAll();
+    public void saveAndFindAllTest() throws Exception {
+        userRepository.saveAll(userList);
 
-        Assert.assertNotEquals(list.size(), 1);
+        List<User> checkUserList = userRepository.findAll();
+        Assert.assertEquals(checkUserList.size(), userList.size());
 
-        User checkUser = list.get(0);
-        Assert.assertNotEquals(checkUser.getId(), tempUser.getId());
-        Assert.assertNotEquals(checkUser.getName(), tempUser.getName());
-        Assert.assertNotEquals(checkUser.getPassword(), tempUser.getPassword());
-        Assert.assertNotEquals(checkUser.getEmailAddr(), tempUser.getEmailAddr());
+        int checkIdx = 0;
+        for(; checkIdx < checkUserList.size(); checkIdx++) {
+            Assert.assertEquals(checkUserList.get(checkIdx).toString(),
+                    userList.get(checkIdx).toString());
+        }
     }
 
     @Test
-    public void listUserTest() throws Exception {
+    public void saveAndUpdateTest() throws Exception {
+        User changeUser = new User(userList.get(0));
+        userRepository.saveAll(userList);
 
+        changeUser.setName("changeTest1");
+        changeUser.setEmailAddr("changeTest@test.com");
+        userRepository.save(changeUser);
+
+        User orgUser = userList.get(0);
+        User afterUser = userRepository.findById(orgUser.getId()).orElse(null);
+
+        Assert.assertNotNull(afterUser);
+        Assert.assertNotEquals(afterUser.getName(), orgUser.getName());
+        Assert.assertNotEquals(afterUser.getEmailAddr(), orgUser.getEmailAddr());
     }
 
     @Test
-    public void updateUserTest() throws Exception {
+    public void saveAndDeleteTest() throws Exception {
+        List<User> checkList1 = userRepository.saveAll(userList);
+        userRepository.delete(userList.get(0));
 
-    }
+        List<User> checkList2 = userRepository.findAll();
 
-    @Test
-    public void deleteUserTest() throws Exception {
-
+        Assert.assertNotEquals(checkList1.size(), checkList2.size());
     }
 }
