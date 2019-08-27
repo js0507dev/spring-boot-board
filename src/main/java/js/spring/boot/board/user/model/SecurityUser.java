c2292d66cfbf88c97b496580c54e513f896b4996
js0507dev <js0507dev@gmail.com>
2019-08-27 23:00:13 +0900

    로그인 기능 추가

---
+++ src/main/java/js/spring/boot/board/user/model/SecurityUser.java
@@ -0,0 +1,25 @@
+package js.spring.boot.board.user.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class SecurityUser extends org.springframework.security.core.userdetails.User {
+    private static final String ROLE_PREFIX = "ROLE_";
+    private static final long serialVersionUID = 1L;
+
+    public SecurityUser(js.spring.boot.board.user.model.User user) {
+        super(user.getId(), user.getPassword(), makeGrantedAuthority(user.getRoles()));
+    }
+
+    private static List<GrantedAuthority> makeGrantedAuthority(List<UserRole> roles){
+        List<GrantedAuthority> list = new ArrayList<>();
+        roles.forEach(role -> list.add(new SimpleGrantedAuthority(ROLE_PREFIX + role.getRoleName())));
+        return list;
+    }
+}
--- src/main/java/js/spring/boot/board/user/service/UserService.java
+++ src/main/java/js/spring/boot/board/user/service/UserService.java
@@ -1,6 +1,8 @@
 package js.spring.boot.board.user.service;
 
+import js.spring.boot.board.user.model.SecurityUser;
 import js.spring.boot.board.user.model.User;
+import js.spring.boot.board.user.model.UserRole;
 import js.spring.boot.board.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,8 +11,16 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
 @Service
 public class UserService implements UserDetailsService {
+    private static final String ROLE_PREFIX = "ROLE_";
+
     @Autowired
     UserRepository userRepository;
 
@@ -18,14 +28,44 @@ public class UserService implements UserDetailsService {
     PasswordEncoder passwordEncoder;
 
     public User createUser(User user) {
-        if(user == null || !user.validate()) {
+        if(user == null || !validateUser(user)) {
             return null;
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRegDate(LocalDate.now());
+        user.setRoles(Arrays.asList(new UserRole(user.getId(), ROLE_PREFIX + "USER")));
         return userRepository.save(user);
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+
+        return Optional.ofNullable(userRepository.findById(id))
+                .filter(u -> u != null)
+                .map(u -> new SecurityUser(u.get())).get();
+    }
+
+    private boolean validateUser(User user) {
+        String id = user.getId();
+        String name = user.getName();
+        String emailAddr = user.getEmailAddr();
+        String password = user.getPassword();
+        if(id == null || id.length() == 0 ||
+                !Pattern.compile("^([a-zA-Z])[0-9a-zA-Z]+$").matcher(id).find()) {
+            return false;
+        }
+        if(name == null || name.length() == 0 ||
+                !Pattern.compile("[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+").matcher(name).find()) {
+            return false;
+        }
+        if(emailAddr != null &&
+                !Pattern.compile("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$").matcher(emailAddr).find()) {
+            return false;
+        }
+        if(password == null || password.length() == 0 ||
+                !Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$").matcher(password).find()) {
+            return false;
+        }
+        return true;
     }
 }
