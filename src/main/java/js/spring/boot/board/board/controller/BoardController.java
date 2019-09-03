085273d4d2332e3f3d8d1e84ce7f25fc5c22fd3b
js0507dev <js0507dev@gmail.com>
2019-09-03 22:47:00 +0900

    템플릿 적용중

--- src/main/java/js/spring/boot/board/board/controller/BoardController.java
+++ src/main/java/js/spring/boot/board/board/controller/BoardController.java
@@ -5,25 +5,22 @@ import js.spring.boot.board.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 
-@RestController
+@Controller
 @RequestMapping("/board")
 public class BoardController {
   @Autowired
   private BoardService boardService;
 
   @GetMapping("")
+  @ResponseBody
   public Page<Board> selectAll(final Pageable pageable, HttpServletResponse res) throws Exception {
     return boardService.findAll(pageable);
-  }
-
-  @GetMapping("/test")
-  public String test() {
-    return "test";
   }
 }
