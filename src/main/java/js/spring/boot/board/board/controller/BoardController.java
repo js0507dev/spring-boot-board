8531ec71fa2b4f48373bae99b0880b049e674bcd
js0507dev <js0507dev@gmail.com>
2019-09-03 09:30:26 +0900

    템플릿 적용중

--- src/main/java/js/spring/boot/board/board/controller/BoardController.java
+++ src/main/java/js/spring/boot/board/board/controller/BoardController.java
@@ -1,19 +1,29 @@
 package js.spring.boot.board.board.controller;
 
-import org.springframework.stereotype.Controller;
+import js.spring.boot.board.board.model.Board;
+import js.spring.boot.board.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Controller
+@RestController
 @RequestMapping("/board")
 public class BoardController {
-    private static final String TEMPLATE_PREFIX = "board/";
-    @GetMapping("/{id}")
-    public String selectOne(HttpServletRequest req,
-                       ModelAndView model) throws Exception {
-        return TEMPLATE_PREFIX + "singlePage";
-    }
+  @Autowired
+  private BoardService boardService;
+
+  @GetMapping("")
+  public Page<Board> selectAll(final Pageable pageable, HttpServletResponse res) throws Exception {
+    return boardService.findAll(pageable);
+  }
+
+  @GetMapping("/test")
+  public String test() {
+    return "test";
+  }
 }
--- src/main/java/js/spring/boot/board/board/repository/BoardRepository.java
+++ src/main/java/js/spring/boot/board/board/repository/BoardRepository.java
@@ -1,7 +1,9 @@
 package js.spring.boot.board.board.repository;
 
+import js.spring.boot.board.board.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository {//} extends PagingAndSortingRepository {
+public interface BoardRepository extends JpaRepository<Board, Long> {//} extends PagingAndSortingRepository {
 }
